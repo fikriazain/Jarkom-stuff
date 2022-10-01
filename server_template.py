81f1ef9910c4486d2432b9a93d947fa58cddaa9e
Fikri Aufaa Zain <fikri.azain@gmail.com>
2022-10-01 09:43:26 +0000

    can access using dig command

--- server_template.py
+++ server_template.py
@@ -6,6 +6,9 @@ SERVER_PORT = 4040
 BUFFER_SIZE = 4096
 SERVER_IP =  ""
 
+IP_ASDOS = "34.101.92.60"
+UDP_ASDOS = 5353
+
 def request_parser(request_message_raw: bytearray, source_address: Tuple[str, int]) -> str:
     # Put your request message decoding logic here.
     # This method return a str.
@@ -20,17 +23,33 @@ def response_parser(response_mesage_raw: bytearray) -> str:
     # TIDAK MENGUBAH ATAUPUN MENGHAPUS  SPEC (parameter dan return type) YANG DIMINTA.
     pass
 
+def socket_handler(
+    sc: socket.socket, inbound_message_raw: bytearray, source_addr: Tuple[str, int]
+):
+    inbound_message = inbound_message_raw
+
+    print(f"Menerima input dari {source_addr}: {inbound_message}")
+    outgoing_message = inbound_message_raw
+    outgoing_message_raw = outgoing_message
+    sc.sendto(outgoing_message_raw, source_addr)
+
+
 def main():
     # Put the rest of your program's logic here (socket etc.). 
     # Pastikan blok socket Anda berada pada fungsi ini.
-	sc = socket.socket()
-	sc.bind((SERVER_IP, SERVER_PORT))
-	sc.listen(2)
-	while True:
-		connection, address = sc.accept()
-		print(f"Menerima address: {address}")
-		connection.close()
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sc:
+        sc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sc.bind((SERVER_IP, SERVER_PORT))
 
+        print("Contoh Socket Server UDP Multithreaded")
+        print("Tekan Ctrl/CMD+C untuk menghentikan program")
+
+        while True:
+            inbound_message_raw, source_addr = sc.recvfrom(BUFFER_SIZE)
+            thread_job = threading.Thread(
+                target=socket_handler, args=(sc, inbound_message_raw, source_addr)
+            )
+            thread_job.start()
 # DO NOT ERASE THIS PART!
 if __name__ == "__main__":
     main() 
