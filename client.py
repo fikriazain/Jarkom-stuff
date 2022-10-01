459fbe47797cb391c8bb214737b6938d86aee812
Fikri Aufaa Zain <fikri.azain@gmail.com>
2022-10-01 07:29:45 +0000

    client.py paste

--- client.py
+++ client.py
@@ -4,12 +4,12 @@ SERVER_IP = "35.223.158.134"
 SERVER_PORT = 4040
 
 def main():
-	sc.socket(socket.AF_INET, socket.SOCK_STREAM)
-	sc.connect((SERVER_IP, SERVER_PORT))
-	input_value = input("Input: ")
-	bytess = input_value.encode("UTF-8")
-	sc.send(bytess)
-	sc.close()
+        sc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sc.connect((SERVER_IP, SERVER_PORT))
+        input_value = input("Input: ")
+        bytess = input_value.encode("UTF-8")
+        sc.send(bytess)
+        sc.close()
 
-if __name__ = "__main__":
-	main()
+if __name__ == "__main__":
+        main()
